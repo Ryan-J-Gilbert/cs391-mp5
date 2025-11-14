79889e61b82dff48369fc53148399b7f8462e886
Ryan Gilbert <ryanjg@bu.edu>
2025-11-14 14:49:55 -0500

    moved log for testing

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { MongoClient, Db, Collection } from "mongodb";
 
 const MONGO_URI = process.env.MONGO_URI as string;
-console.log(MONGO_URI.substring(0,5));
+
 
 if (!MONGO_URI) {
     throw new Error("NO API key");
@@ -23,6 +23,7 @@ async function connect(): Promise<Db>{
 
 }
 export default async function getCollection(collectionName: string): Promise<Collection>{
+    console.log(MONGO_URI.substring(0,5));
     if (!db){
         db = await connect();
     }
